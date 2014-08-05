7860a6afa64937e290972aa30ae37d23a70805f6
liamjbennett <lbennett@opentable.com>
2014-08-05 13:40:00 +0100

    fixing config

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,7 +2,8 @@ class exhibitor::config(
   $opts = $exhibitor::opts,
   $properties = $exhibitor::properties,
   $zk_data_dir = $exhibitor::zk_data_dir,
-  $zk_quorum = $exhibitor::zk_quorum
+  $zk_quorum = $exhibitor::zk_quorum,
+  $install_dir = $exhibitor::install_dir
 ) {
 
   file { '/etc/default/exhibitor':
@@ -10,7 +11,7 @@ class exhibitor::config(
     content => template('exhibitor/default.erb')
   }
   
-  file { "${exhibtor::install_dir}/exhibitor.properies":
+  file { "${install_dir}/exhibitor.properies":
     ensure  => present,
     content => template('exhibitor/exhibitor.properties.erb') 
   }
